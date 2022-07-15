6a41f703f4db472b3b50ab0313426aa5b2fec53a
EpsilonTheDerg <jamesclamey@gmail.com>
2022-07-15 19:53:43 -0400

    no message

--- Assets/Scripts/DieManager.cs
+++ Assets/Scripts/DieManager.cs
@@ -27,6 +27,9 @@ public class DieManager:MonoBehaviour {
     private const float LERP_VALUE = 15;
     private const float SPACE_BUFFER = 32;
 
+    private int managerState = 0; // 0 = waiting, 1 = rolling
+    private int lastDieRolled = 0; // 0 = move, 1 = turn, 2 = action
+
     public void Start() {
         queueBar = transform.GetChild(0).gameObject;
         queueImg = queueBar.GetComponent<Image>();
@@ -42,39 +45,69 @@ public class DieManager:MonoBehaviour {
         queueRect.sizeDelta = Vector2.Lerp(queueRect.sizeDelta, new Vector2(dieSize.x * maxRolls + SPACE_BUFFER * (maxRolls + 1), dieSize.y), LERP_VALUE * Time.deltaTime);
         for (int i = 0; i < dice.Count; i++) {
             //dice[i].dieRect.position = Vector2.Lerp(dice[i].dieRect.position, new Vector2(queueRect.position.x - (queueRect.sizeDelta.x * 0.5f) +
-            //    (SPACE_BUFFER * i) + (dieSize.x * (i - 1)) + (dieSize.x * 0.5f), queueRect.position.y), LERP_VALUE * Time.deltaTime);
-            dice[i].dieRect.position = Vector2.Lerp(dice[i].dieRect.position, new Vector2(queueRect.position.x - ((SPACE_BUFFER + dieSize.x) *(dice.Count - 1) * 0.5f) +
-                (SPACE_BUFFER * i) + (dieSize.x * (i - 1)) + (dieSize.x * 0.5f), queueRect.position.y), LERP_VALUE * Time.deltaTime);
+            //    (SPACE_BUFFER * (i + 1)) + (dieSize.x * i) + (dieSize.x * 0.5f), queueRect.position.y), LERP_VALUE * Time.deltaTime);
+            dice[i].dieRect.position = Vector2.Lerp(dice[i].dieRect.position, new Vector2(queueRect.position.x - ((SPACE_BUFFER + dieSize.x) * dice.Count * 0.5f) +
+                (SPACE_BUFFER * (i + 1)) + (dieSize.x * i) + (dieSize.x * 0.5f) - (SPACE_BUFFER * 0.5f), queueRect.position.y), LERP_VALUE * Time.deltaTime);
+        }
+
+        if (managerState == 1 && !dieRoller.isRolling()) {
+            AddDie(new Vector2(lastDieRolled, dieRoller.curside));
+            managerState = 0;
         }
     }
 
-    public void AddDie(string name, Vector2 data) {
+    public void Roll(int mode) {
+        if (managerState == 0 && dice.Count < maxRolls) {
+            dieRoller.roll(mode);
+            managerState = 1;
+            lastDieRolled = mode;
+        }
+    }
+
+    public void AddDie(Vector2 data) {
         GameObject newDieObj = Instantiate(dieObj);
+        newDieObj.transform.SetParent(transform);
         Die newDie = new Die { dieObj = newDieObj, dieData = data };
         newDie.dieRect = newDie.dieObj.GetComponent<RectTransform>();
         newDie.dieImg = newDie.dieObj.GetComponent<Image>();
-        newDie.dieImg.sprite = GetSprite(name);
+        newDie.dieImg.sprite = GetSprite(data);
         newDie.dieRect.position = new Vector2(queueRect.position.x, queueRect.position.y + 512);
         dice.Add(newDie);
     }
+    //public void AddDie(string name, Vector2 data) {
+    //    GameObject newDieObj = Instantiate(dieObj);
+    //    Die newDie = new Die { dieObj = newDieObj, dieData = data };
+    //    newDie.dieRect = newDie.dieObj.GetComponent<RectTransform>();
+    //    newDie.dieImg = newDie.dieObj.GetComponent<Image>();
+    //    newDie.dieImg.sprite = GetSprite(name);
+    //    newDie.dieRect.position = new Vector2(queueRect.position.x, queueRect.position.y + 512);
+    //    dice.Add(newDie);
+    //}
 
-    private Sprite GetSprite(string name) {
-        return spriteLib[name.ToLower().Replace(" ", "") switch {
-            "move1" => 0,
-            "move2" => 1,
-            "move3" => 2,
-            "move4" => 3,
-            "move5" => 4,
-            "move6" => 5,
-            "turncw" => 6,
-            "turnccw" => 7,
-            "use" => 8,
-            "sword" => 9,
-            "360" => 10,
-            "bow" => 11,
-            "pickup" => 12,
-            "set" => 13,
-            _ => 0
+    //private Sprite GetSprite(string name) {
+    //    return spriteLib[name.ToLower().Replace(" ", "") switch {
+    //        "move1" => 0,
+    //        "move2" => 1,
+    //        "move3" => 2,
+    //        "move4" => 3,
+    //        "move5" => 4,
+    //        "move6" => 5,
+    //        "turncw" => 6,
+    //        "turnccw" => 7,
+    //        "use" => 8,
+    //        "sword" => 9,
+    //        "360" => 10,
+    //        "bow" => 11,
+    //        "pickup" => 12,
+    //        "set" => 13,
+    //        _ => 0
+    //    }];
+    //}
+    private Sprite GetSprite(Vector2 data) {
+        return spriteLib[data.x switch {
+            0 => data.y switch { 1 => 0, 2 => 1, 3 => 2, 4 => 3, 5 => 4, _ => 5 },
+            1 => data.y switch { 1 => 7, 2 => 7, 3 => 6, 4 => 6, 5 => 6, _ => 7 },
+            _ => data.y switch { 1 => 9, 2 => 11, 3 => 12, 4 => 13, 5 => 10, _ => 8 }
         }];
     }
 }
