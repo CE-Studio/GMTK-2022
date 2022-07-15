59ba8ccee91352a84e0c8feaa12a063f8c5c6152
clarence112 <clarencepacelli@gmail.com>
2022-07-15 17:10:41 -0400

    no message

--- Assets/Scripts/physdieController.cs
+++ Assets/Scripts/physdieController.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class physdieController : MonoBehaviour {
 
+    public Material move;
+    public Material rotate;
+    public Material action;
 
+    public Rigidbody rb;
+    public MeshRenderer mr;
+
+    public int curmode = 0;
 
     void Start() {
-        
+        roll(1);
     }
 
     void Update() {
         
+    }
+
+    public void roll(int mode) {
+        gameObject.SetActive(true);
+        curmode = mode;
+        switch (mode) {
+            default:
+                mr.material = move;
+                break;
+            case 1:
+                mr.material = rotate;
+                break;
+            case 2:
+                mr.material = action;
+                break;
+        }
+        transform.localPosition = new Vector3(0, 0, 1);
+        rb.AddTorque(new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f)));
+        rb.AddForce(new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f)));
     }
 }
