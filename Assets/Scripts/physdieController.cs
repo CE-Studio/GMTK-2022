3a983215f7bc488d6a06bf1caf1245924c7e36b8
clarence112 <clarencepacelli@gmail.com>
2022-07-15 18:36:05 -0400

    no message

--- Assets/Scripts/physdieController.cs
+++ Assets/Scripts/physdieController.cs
@@ -12,17 +12,47 @@ public class physdieController : MonoBehaviour {
     public MeshRenderer mr;
 
     public int curmode = 0;
+    public int curside = 0;
+
+    public bool debug;
 
     void Start() {
         gameObject.SetActive(false);
         transform.localPosition = new Vector3(0, 0, -1);
-        roll(0);
+        if (debug) {
+            roll(0);
+        }
     }
 
     void Update() {
         if (rb.IsSleeping()) {
+            findSide();
             Start();
         }
+    }
+
+    private void findSide() {
+
+        Vector3[] sidelist = {
+                                  transform.forward,
+                                  transform.right,
+                                  -transform.forward,
+                                  -transform.up,
+                                  -transform.right,
+                                  transform.up,
+                                  };
+
+        int h = 0;
+        for (int i = 0; i < 6; i++) {
+            h = i;
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, sidelist[i], out hit, 0.6f)) {
+                break;
+            }
+        }
+        h += 1;
+        print(h);
+        curside = h;
     }
 
     public void roll(int mode) {
@@ -43,5 +73,9 @@ public class physdieController : MonoBehaviour {
         transform.localPosition = new Vector3(0, 0, 1);
         rb.AddTorque(new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f)));
         rb.AddForce(new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f)));
+    }
+
+    public bool isRolling() {
+        return (!rb.IsSleeping());
     }
 }
