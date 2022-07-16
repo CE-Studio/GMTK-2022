d39397e4063e9874d099f6d2528b2b726e37bed2
EpsilonTheDerg <jamesclamey@gmail.com>
2022-07-15 21:26:53 -0400

    no message

--- Assets/Scripts/DieManager.cs
+++ Assets/Scripts/DieManager.cs
@@ -23,15 +23,14 @@ public class DieManager:MonoBehaviour {
 
     public Sprite[] spriteLib;
 
-    private Vector2 dieSize = new Vector2(128, 128);
+    private Vector2 dieSize = Vector2.zero;
     private const float LERP_VALUE = 15;
-    private const float SPACE_BUFFER = 0;
+    private const float SPACE_BUFFER = 32;
 
     private int managerState = 0; // 0 = waiting, 1 = rolling
     private int lastDieRolled = 0; // 0 = move, 1 = turn, 2 = action
 
     public void Start() {
-        //dieSize = dieObj.GetComponent<RectTransform>().sizeDelta;
         queueBar = transform.GetChild(0).gameObject;
         queueImg = queueBar.GetComponent<Image>();
         queueRect = queueBar.GetComponent<RectTransform>();
@@ -39,15 +38,17 @@ public class DieManager:MonoBehaviour {
         queueRect.anchorMax = new Vector2(0.5f, 0);
 
         dieObj = Resources.Load<GameObject>("Objects/Die");
+
+        dieSize = dieObj.GetComponent<RectTransform>().sizeDelta;
     }
 
     public void Update() {
         queueRect.sizeDelta = Vector2.Lerp(queueRect.sizeDelta, new Vector2(dieSize.x * maxRolls + SPACE_BUFFER * (maxRolls + 1), dieSize.y), LERP_VALUE * Time.deltaTime);
         for (int i = 0; i < dice.Count; i++) {
-            //dice[i].dieRect.position = Vector2.Lerp(dice[i].dieRect.position, new Vector2(queueRect.position.x - (queueRect.sizeDelta.x * 0.5f) +
-            //    (SPACE_BUFFER * (i + 1)) + (dieSize.x * i) + (dieSize.x * 0.5f), queueRect.position.y), LERP_VALUE * Time.deltaTime);
-            dice[i].dieRect.position = Vector2.Lerp(dice[i].dieRect.position, new Vector2(queueRect.position.x - ((SPACE_BUFFER + dieSize.x) * dice.Count * 0.5f) +
-                (SPACE_BUFFER * (i + 1)) + (dieSize.x * i) + (dieSize.x * 0.5f) - (SPACE_BUFFER * 0.5f), queueRect.position.y), LERP_VALUE * Time.deltaTime);
+            if (dieSize == Vector2.zero)
+                dieSize = new Vector2(dice[i].dieRect.rect.width, dice[i].dieRect.rect.height);
+            dice[i].dieRect.localPosition = Vector2.Lerp(dice[i].dieRect.localPosition, new Vector2(queueRect.localPosition.x - ((SPACE_BUFFER + dieSize.x) * dice.Count * 0.5f) +
+                (SPACE_BUFFER * (i + 1)) + (dieSize.x * i) + (dieSize.x * 0.5f) - (SPACE_BUFFER * 0.5f), queueRect.localPosition.y), LERP_VALUE * Time.deltaTime);
         }
 
         if (managerState == 1 && !dieRoller.isRolling()) {
@@ -70,38 +71,10 @@ public class DieManager:MonoBehaviour {
         newDie.dieRect = newDie.dieObj.GetComponent<RectTransform>();
         newDie.dieImg = newDie.dieObj.GetComponent<Image>();
         newDie.dieImg.sprite = GetSprite(data);
-        newDie.dieRect.position = new Vector2(queueRect.position.x, queueRect.position.y + 512);
+        newDie.dieRect.localPosition = new Vector2(queueRect.localPosition.x, queueRect.localPosition.y + 512);
         dice.Add(newDie);
     }
-    //public void AddDie(string name, Vector2 data) {
-    //    GameObject newDieObj = Instantiate(dieObj);
-    //    Die newDie = new Die { dieObj = newDieObj, dieData = data };
-    //    newDie.dieRect = newDie.dieObj.GetComponent<RectTransform>();
-    //    newDie.dieImg = newDie.dieObj.GetComponent<Image>();
-    //    newDie.dieImg.sprite = GetSprite(name);
-    //    newDie.dieRect.position = new Vector2(queueRect.position.x, queueRect.position.y + 512);
-    //    dice.Add(newDie);
-    //}
 
-    //private Sprite GetSprite(string name) {
-    //    return spriteLib[name.ToLower().Replace(" ", "") switch {
-    //        "move1" => 0,
-    //        "move2" => 1,
-    //        "move3" => 2,
-    //        "move4" => 3,
-    //        "move5" => 4,
-    //        "move6" => 5,
-    //        "turncw" => 6,
-    //        "turnccw" => 7,
-    //        "use" => 8,
-    //        "sword" => 9,
-    //        "360" => 10,
-    //        "bow" => 11,
-    //        "pickup" => 12,
-    //        "set" => 13,
-    //        _ => 0
-    //    }];
-    //}
     private Sprite GetSprite(Vector2 data) {
         return spriteLib[data.x switch {
             0 => data.y switch { 1 => 0, 2 => 1, 3 => 2, 4 => 3, 5 => 4, _ => 5 },
