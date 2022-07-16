0ddc453efdc09e2b46f3640a5b6b32811ca8dde4
clarence112 <clarencepacelli@gmail.com>
2022-07-15 23:06:13 -0400

    no message

--- Assets/Scripts/playerControler.cs
+++ Assets/Scripts/playerControler.cs
@@ -20,14 +20,21 @@ public class playerControler : MonoBehaviour {
 
     void Start() {
         transform.localPosition = new Vector3(Mathf.Floor(transform.localPosition.x) + 0.5f, Mathf.Floor(transform.localPosition.y) + 0.5f, transform.localPosition.z);
+        print(isTraversable(Vector2Int.zero));
     }
 
     void Update() {
         
     }
 
+    bool isLocallyTraversable(Vector2Int pos) {
+        Vector3Int h = Vector3Int.FloorToInt(transform.localPosition);
+        return (isTraversable(new Vector2Int(h.x + pos.x, h.y + pos.y)));
+    }
+
     bool isTraversable(Vector2Int pos) {
         Tile tile = playarea.GetTile<Tile>(new Vector3Int(pos.x, pos.y, 0));
+        if (tile == null) return true;
         print(tile.name);
         return (Array.IndexOf(traversable, tile.name) > -1);
     }
