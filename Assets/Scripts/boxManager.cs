38478e459a4847aff2363b280c8e140b7d7fc809
clarence112 <clarencepacelli@gmail.com>
2022-07-16 04:37:47 -0400

    no message

---
+++ Assets/Scripts/boxManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxManager : MonoBehaviour {
+
+    public boxControler[] boxes;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
