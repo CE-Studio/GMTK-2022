33a35c31849396ce0687f2392bb40f5f1c53392c
EpsilonTheDerg <jamesclamey@gmail.com>
2022-07-16 21:48:24 -0400

    no message

--- Assets/Scripts/boxControler.cs
+++ Assets/Scripts/boxControler.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class boxControler : MonoBehaviour {
 
     public bool beingHeld = false;
+    public GameObject shadow;
 
     void Start() {
         transform.localPosition = new Vector3(Mathf.Floor(transform.localPosition.x) + 0.5f, Mathf.Floor(transform.localPosition.y) + 0.5f, transform.localPosition.z);
+        shadow = transform.GetChild(0).gameObject;
         boxManager.boxes.Add(this);
     }
 
     void Update() {
         if (beingHeld) {
             transform.localPosition = playerControler.thePlayer.transform.localPosition + new Vector3(0, 0.87f, 0);
+            shadow.SetActive(false);
         } else {
             transform.localPosition = new Vector3(Mathf.Floor(transform.localPosition.x) + 0.5f, Mathf.Floor(transform.localPosition.y) + 0.5f, transform.localPosition.z);
+            shadow.SetActive(true);
         }
     }
 }
